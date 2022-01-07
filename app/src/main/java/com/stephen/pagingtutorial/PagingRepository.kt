fb72785a68fe63240b1216d5b052f3f18ba83d25
StephenLeeDev <stephen.lee.dev@gmail.com>
2022-01-07 23:00:43 +0900

    Add | Generate PagingRepository

---
+++ app/src/main/java/com/stephen/pagingtutorial/PagingRepository.kt
@@ -0,0 +1,16 @@
+package com.stephen.pagingtutorial
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class PagingRepository @Inject constructor(private val service: SampleBackendService) {
+
+    fun getPagingData(): Flow<PagingData<String>> {
+        return Pager(PagingConfig(pageSize = 10)) {
+            SamplePagingSource(service)
+        }.flow
+    }
+}
